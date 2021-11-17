3aa208ec7db38a28bb5fd793f21ac39515deedb6
David <ddaarr@live.com.pt>
2021-11-17 09:16:26 +0000

    movies not working

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,11 +1,29 @@
 const router = require("express").Router();
+const Celebrity = require("../models/celebrity.model");
 const Movie = require("./../models/Movie.model");
 
 
-//PAGE TO CREATE A NEW CELEBRITY
+//PAGE TO CREATE A NEW MOVIE
 router.get("/movies/create", (req, res) => {
-  res.render('movies/new-movie');
+  Celebrity.find()
+  .then((celebritiesFound) => {
+    res.render('movies/new-movie', {celebritiesFound: celebritiesFound});
+    console.log(celebritiesFound)
+  }).catch((err) => {
+    console.log(err);
+  });
 });
+
+router.post("/movies/create", (req, res) => {
+  const { title, genre, plot, cast } = req.body;
+  
+  Movie.create({ title, genre, plot, cast })
+      .then((newMovie) => {
+          res.redirect(`/movies`);
+      })
+      .catch( (err) => console.log(err));
+  
+  });
 
 
 
